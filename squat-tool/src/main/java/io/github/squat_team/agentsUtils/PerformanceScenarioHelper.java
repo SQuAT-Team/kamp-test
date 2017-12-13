9b4cbc13a9ce931eb7360737bfa98ed747ced850
franksn90 <s.frankausowen@googlemail.com>
2017-12-13 14:00:49 -0300

    Add performance scenarios for Cocome

--- squat-tool/src/main/java/io/github/squat_team/agentsUtils/PerformanceScenarioHelper.java
+++ squat-tool/src/main/java/io/github/squat_team/agentsUtils/PerformanceScenarioHelper.java
@@ -15,6 +15,8 @@ import io.github.squat_team.model.PCMResult;
 import io.github.squat_team.model.ResponseMeasureType;
 import io.github.squat_team.performance.AbstractPerformancePCMScenario;
 import io.github.squat_team.performance.PerformanceMetric;
+import io.github.squat_team.performance.PerformancePCMCPUScenario;
+import io.github.squat_team.performance.PerformancePCMUsageScenario;
 import io.github.squat_team.performance.PerformancePCMWokloadScenario;
 import io.github.squat_team.performance.peropteryx.PerOpteryxPCMBot;
 import io.github.squat_team.performance.peropteryx.configuration.ConfigurationImprovedImproved;
@@ -37,7 +39,7 @@ public class PerformanceScenarioHelper {
 	public static AbstractPerformancePCMScenario createScenarioOfCPU() {
 		ArrayList<String> cpuIDs = new ArrayList<String>();
 		cpuIDs.add(TestConstants.CPU_ID);
-		AbstractPerformancePCMScenario scenario = new PerformancePCMWokloadScenario(OptimizationType.MINIMIZATION,
+		AbstractPerformancePCMScenario scenario = new PerformancePCMCPUScenario(OptimizationType.MINIMIZATION,
 				cpuIDs, 0.5);
 		PCMResult expectedResponse = new PCMResult(ResponseMeasureType.DECIMAL);
 		expectedResponse.setResponse(6.0);
@@ -45,6 +47,59 @@ public class PerformanceScenarioHelper {
 		scenario.setMetric(PerformanceMetric.RESPONSE_TIME);
 		return scenario;
 	}
+	
+	// COCOME
+	
+	public static AbstractPerformancePCMScenario createScenario1Cocome() {
+		ArrayList<String> workloadIDs = new ArrayList<String>();
+		workloadIDs.add("_VgwxwHr3Eeek77WF10mCCg");
+		AbstractPerformancePCMScenario scenario = new PerformancePCMWokloadScenario(OptimizationType.MINIMIZATION,
+				workloadIDs, 1.1);
+		PCMResult expectedResponse = new PCMResult(ResponseMeasureType.DECIMAL);
+		expectedResponse.setResponse(6.0);
+		scenario.setExpectedResponse(expectedResponse);
+		scenario.setMetric(PerformanceMetric.RESPONSE_TIME);
+		return scenario;
+	}
+	
+	public static AbstractPerformancePCMScenario createScenario2Cocome() {
+		ArrayList<String> workloadIDs = new ArrayList<String>();
+		workloadIDs.add("_VgwxwHr3Eeek77WF10mCCg");
+		AbstractPerformancePCMScenario scenario = new PerformancePCMWokloadScenario(OptimizationType.MINIMIZATION,
+				workloadIDs, 1.5);
+		PCMResult expectedResponse = new PCMResult(ResponseMeasureType.DECIMAL);
+		expectedResponse.setResponse(6.0);
+		scenario.setExpectedResponse(expectedResponse);
+		scenario.setMetric(PerformanceMetric.RESPONSE_TIME);
+		return scenario;
+	}
+	
+	public static AbstractPerformancePCMScenario createScenario3Cocome() {
+		ArrayList<String> cpuIDs = new ArrayList<String>();
+		cpuIDs.add("_WV4YUK2VEeaxN4gXuIkS2A");
+		AbstractPerformancePCMScenario scenario = new PerformancePCMCPUScenario(OptimizationType.MINIMIZATION,
+				cpuIDs, 0.5);
+		PCMResult expectedResponse = new PCMResult(ResponseMeasureType.DECIMAL);
+		expectedResponse.setResponse(6.0);
+		scenario.setExpectedResponse(expectedResponse);
+		scenario.setMetric(PerformanceMetric.RESPONSE_TIME);
+		return scenario;
+	}
+	
+	public static AbstractPerformancePCMScenario createScenario4Cocome() {
+		String loopIterations =  "IntPMF[(1; 0.01)(2; 0.01)(3; 0.02)(4; 0.02)(5; 0.03)(6; 0.03)(7; 0.04)(8; 0.04)(9; 0.05)(10; 0.06)"
+				+ "(11; 0.06)(12; 0.06)(13; 0.08)(14; 0.09)(15; 0.10)(16; 0.09)(17; 0.07)(18; 0.06)(19; 0.05)(20; 0.03)]";;
+		ArrayList<String> loopIDs = new ArrayList<String>();
+		loopIDs.add("_fsG44tqFEee4ToXBRRujSw");
+		AbstractPerformancePCMScenario scenario = new PerformancePCMUsageScenario(OptimizationType.MINIMIZATION,
+				loopIDs, loopIterations);
+		PCMResult expectedResponse = new PCMResult(ResponseMeasureType.DECIMAL);
+		expectedResponse.setResponse(6.0);
+		scenario.setExpectedResponse(expectedResponse);
+		scenario.setMetric(PerformanceMetric.RESPONSE_TIME);
+		return scenario;
+	}
+	
 
 	public static PerOpteryxPCMBot createPCMBot(AbstractPerformancePCMScenario scenario) {
 		// create configuration
